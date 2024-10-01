6c7207c19cfb4fe3ae817ae9a978d3833a20d13b
ThiKieuDungNgo <thikieun@uci.edu>
2024-10-01 12:42:52 -0700

    Update and rename Gender_Gene_Expression_b.R to Gender_Gene_Expression.R

--- code/Gender_Gene_Expression.R
+++ code/Gender_Gene_Expression.R
@@ -1,4 +1,4 @@
-
+library(DESeq2)
 library(limma)
 library(edgeR)
 library(dplyr)
@@ -8,15 +8,28 @@ library(stringr)
 file_path <- "C:/Users/salab/Desktop/UCI_project/PROJECT/Transcriptome/Combined_Expression_Gender.csv"
 df <- read.csv(file_path)
 
-df$Gender <- str_trim(df$Gender)
-df$Group <- str_trim(df$Group)
+df$Group <- trimws(df$Group)
+df$Gender <- trimws(df$Gender)
 
+df$Group <- factor(df$Group, levels = c("Control", "Disease"))
+df$Gender <- factor(df$Gender)
 df$Sample <- as.factor(df$Sample)
 
 exprs <- as.matrix(df[, -(1:3)])  # Remove Sample, Gender, and Group columns
 rownames(exprs) <- df$Sample
-
 exprs <- t(exprs)
+
+count_data <- round(exprs)  # DESeq2 requires integer counts
+
+dds <- DESeqDataSetFromMatrix(countData = count_data, colData = df, design = ~ Group)
+
+dds <- DESeq(dds)
+
+res_deseq <- results(dds)
+
+res_deseq <- res_deseq[order(res_deseq$padj), ]
+
+res_sig_deseq <- res_deseq[which(res_deseq$padj < 0.05), ]
 
 y <- DGEList(counts = exprs)
 y <- calcNormFactors(y)
@@ -26,10 +39,6 @@ plotMDS(norm_exprs, labels = df$Group, col = as.numeric(df$Gender))
 
 design <- model.matrix(~ 0 + Group:Gender, data = df)
 colnames(design) <- make.names(colnames(design))
-
-print(colnames(design))
-
-v <- voomWithQualityWeights(y, design, plot = TRUE)
 
 contrast.matrix <- makeContrasts(
   MaleControl_vs_MaleDisease = GroupDisease.GenderMale - GroupControl.GenderMale,
@@ -41,16 +50,17 @@ contrast.matrix <- makeContrasts(
 )
 
 fit <- lmFit(norm_exprs, design)
-
 fit2 <- contrasts.fit(fit, contrast.matrix)
 fit2 <- eBayes(fit2)
 
+# Extract the top tables for each comparison
 results_MaleControl_vs_MaleDisease <- topTable(fit2, coef = "MaleControl_vs_MaleDisease", adjust.method = "BH", number = Inf)
 results_FemaleControl_vs_FemaleDisease <- topTable(fit2, coef = "FemaleControl_vs_FemaleDisease", adjust.method = "BH", number = Inf)
 results_FemaleDisease_vs_MaleDisease <- topTable(fit2, coef = "FemaleDisease_vs_MaleDisease", adjust.method = "BH", number = Inf)
 results_FemaleControl_vs_MaleControl <- topTable(fit2, coef = "FemaleControl_vs_MaleControl", adjust.method = "BH", number = Inf)
 results_GenderInteraction <- topTable(fit2, coef = "GenderInteraction", adjust.method = "BH", number = Inf)
 
+# Step 10: Filter significant results (adjusted p-value < 0.05) for limma/voom results
 significant_MaleControl_vs_MaleDisease <- results_MaleControl_vs_MaleDisease %>% filter(adj.P.Val < 0.05)
 significant_FemaleControl_vs_FemaleDisease <- results_FemaleControl_vs_FemaleDisease %>% filter(adj.P.Val < 0.05)
 significant_FemaleDisease_vs_MaleDisease <- results_FemaleDisease_vs_MaleDisease %>% filter(adj.P.Val < 0.05)
